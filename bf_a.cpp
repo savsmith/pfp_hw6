359aabe565a682fb719675b5078d3d38a1a3b2f0
Savannah Smith <savannah@cs.utexas.edu>
2018-04-27 15:02:44 -0500

    Converted to using CAS

--- bf_a.cpp
+++ bf_a.cpp
@@ -7,8 +7,10 @@
 #include <cmath>
 #include <cstdlib>
 #include <limits>
-#include "ittnotify.h" // header for VTune calls
 #include <atomic>
+#include <omp.h>
+
+//#include "ittnotify.h" // header for VTune calls
 
 #include "graph.h"
 
@@ -24,38 +26,34 @@ enum SourceNode {
 class serialBellmanFord {
 	graph &g;
 	SourceNode src;
-	std::atomic<int>* distances;
+	std::atomic<int> *distances;
 
 private:
 	void initialize() {
 		for(int n = g.begin(); n < g.end(); n++) {
-			distances[n] = INT_MAX;
+			distances[n].store(INT_MAX);
 		}
-		distances[src] = 0;
-		//printGraphDistances();
+		distances[src].store(0);
 	}
-	bool converged(int distances[], int dists[]){
-		for(int n = g.begin(); n < g.end(); n++) {
-			if (distances[n] != dists[n])
-				return false; 
+	bool relaxEdge(graph::node_t u, graph::node_t v, graph::edge_t e) {
+		int newDist = distances[u].load() + g.get_edge_data(e);
+		if(distances[u].load() != INT_MAX && newDist < distances[v].load()) {
+			distances[u].store(newDist);
+			return true;
 		}
-		return true;
+		//cout << "** Edge not relaxed, changed bool == false **\n";
+		return false;
 	}
 
-	void copyDistances(int* distances, int* container) {
-		for(int n = g.begin(); n < g.end(); n++) {
-			container[n] = distances[n];
-		}
-	}
 	void printarray(int* array, int size){
 		for(int i = 0; i < size; i++){
 			std::cout << array[i] << std::endl;
 		}
 	}
 
-public:
+public:	
 	serialBellmanFord(graph &g): g(g) { 
-		distances = new int[g.size_nodes()];
+		distances = new std::atomic<int>[g.size_nodes()];
 
 		string type = g.getGraphType();
 		if (type.compare("rmat") == 0) {
@@ -73,33 +71,43 @@ public:
 		}
 	}
 
-	void bellmanFord() {
-  		initialize();
-  		// N - 1 iterations...
-		int prev[g.size_nodes()];
+	void serialBF() {
+		struct timespec tick, tock;         // for measuring runtime
+		uint64_t execTime;                  // time in nanoseconds
+		initialize();
+		bool changed = true;
 
-		__itt_resume();
-  		while(!converged(distances, prev)) {
-  			copyDistances(distances, prev); //Save current state to compare
-	  		for(auto u = g.begin(); u < g.end(); u++) {
-	  			// For each outgoing edge from source u...
-	  			for(auto e = g.edge_begin(u); e < g.edge_end(u); e++) {
-	  				int v = g.get_edge_dst(e);
-	  				int weight = g.get_edge_data(e);
+		//__itt_resume(); // Start measuring runtime here
+		clock_gettime(CLOCK_MONOTONIC_RAW, &tick);
 
-	  				if(distances[u] != INT_MAX && distances[u] + weight < distances[v]) {
-	  					distances[v] = distances[u] + weight;
-	  					/* TODO: Change to CAS */
-	  				}
-	  			}
-	  		}
-  		}
-  		__itt_pause();
-  		printGraphDistances();
+		for(auto i = 0; i < g.size_nodes() - 1; i++) {
+			if(!changed) {
+				break;
+			}
+			changed = false;
+			for(auto u = g.begin(); u < g.end(); u++) {
+				//cout << "Node: " << u << endl;
+				for(auto e = g.edge_begin(u); e < g.edge_end(u); e++) {
+					//cout << "Edge: " << e << endl;
+					graph::node_t v = g.get_edge_dst(e);
+					graph::edge_data_t weight = g.get_edge_data(e);
+					if(relaxEdge(u,v,e)) {
+						changed = true;
+					}
+				}
+			}
+			printGraphDistances();
+		}
+
+		//__itt_pause(); //Stop measuring runtime here
+		clock_gettime(CLOCK_MONOTONIC_RAW, &tock);
+  		execTime = 1000000000 * (tock.tv_sec - tick.tv_sec) + tock.tv_nsec - tick.tv_nsec;
+  		//std::cout << "elapsed process CPU time = " << (long long unsigned int)execTime << " nanoseconds\n";
+
+		printGraphDistances();
 	}
 
 	void printGraphDistances() {
-		std::cout << "Distances" << std::endl;
 		for (auto n = g.begin(); n < g.end(); n++) {
 			if (distances[n] == INT_MAX) {
 				std::cout << n+1 << " INF" << std::endl;
@@ -125,11 +133,11 @@ int main (int argc, char *argv[]) {
 	if(!g.construct_from_dimacs(argv[1])) {
 		return 0;
 	}
-	std::cout << "Graph Name: " << g.getName() << std::endl;
+	//std::cout << "Graph Name: " << g.getName() << std::endl;
 	
 	serialBellmanFord bf(g);
-	std::cout << "Graph size: " << g.size_nodes() << std::endl;
-	bf.bellmanFord();
+	//std::cout << "Graph size: " << g.size_nodes() << std::endl;
+	bf.serialBF();
 
 	return 0;
 }

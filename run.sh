92c00ffbcd7f6eb8bef9ace1a5494aa57224d404
Savannah Smith <savannah@cs.utexas.edu>
2018-04-30 15:18:56 -0500

    mangled code but need to work on my computer. Will recommit later

--- run.sh
+++ run.sh
@@ -1,35 +1,35 @@
 function run {
-	echo "Over Nodes"
-	echo "Dividing Over Nodes" >> "run.out"
-	echo "Graph, Thread Count, schedule type, schedule num, Exec Time (nanoseconds)" >> "run.out"
+	# echo "Over Nodes"
+	# echo "Dividing Over Nodes" >> "run.out"
+	# echo "Graph, Thread Count, schedule type, schedule num, Exec Time (nanoseconds)" >> "run.out"
 
-	for g in wiki-example rmat15 rmat22 roadFLA roadNY
-	do
-		echo -n $g
-		echo -n $g, serial," N/A, N/A, " >> "run.out"
-		./bf_a inputs/$g.dimacs 0 >> "run.out"
-		echo "" >> "run.out"
-		for t in 1 2 4 8
-		do
-			for s in "static" "dynamic"
-			do
-				for n in 1 8 32 128 512
-				do
-					echo -n $g, $t, $s, $n," " >> "run.out"
-					export OMP_SCHEDULE=$s,$n
-					./bf_b inputs/$g.dimacs $t 0 >> "run.out"
-					echo "" >> "run.out"
-					echo -n "."
-				done
-			done
-		done
-		echo "done."
-	done 
-	echo "Over Edges"
-	echo "" >> "run.out"
-	echo "Dividing Over Edges" >> "run.out"
-	echo "Graph, Thread Count, schedule type, schedule num, Exec Time (nanoseconds)" >> "run.out"
-	for g in wiki-example rmat15 rmat22 roadFLA roadNY
+	# for g in wiki-example rmat15 rmat22 roadFLA roadNY
+	# do
+	# 	echo -n $g
+	# 	echo -n $g, serial," N/A, N/A, " >> "run.out"
+	# 	./bf_a inputs/$g.dimacs 0 >> "run.out"
+	# 	echo "" >> "run.out"
+	# 	for t in 1 2 4 8
+	# 	do
+	# 		for s in "static" "dynamic"
+	# 		do
+	# 			for n in 1 8 32 128 512
+	# 			do
+	# 				echo -n $g, $t, $s, $n," " >> "run.out"
+	# 				export OMP_SCHEDULE=$s,$n
+	# 				./bf_b inputs/$g.dimacs $t 0 >> "run.out"
+	# 				echo "" >> "run.out"
+	# 				echo -n "."
+	# 			done
+	# 		done
+	# 	done
+	# 	echo "done."
+	# done 
+	# echo "Over Edges"
+	# echo "" >> "run.out"
+	# echo "Dividing Over Edges" >> "run.out"
+	# echo "Graph, Thread Count, schedule type, schedule num, Exec Time (nanoseconds)" >> "run.out"
+	for g in rmat22 roadFLA roadNY
 	do
 		echo -n $g
 		echo -n $g, serial," N/A, N/A, " >> "run.out"
@@ -55,6 +55,6 @@ function run {
 
 make
 echo "Beginning..."
-echo "Bellman Ford with OpenMP results" > "run.out"
+#echo "Bellman Ford with OpenMP results" > "run.out"
 run
 echo "done."
